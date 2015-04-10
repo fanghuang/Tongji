19b8a9841d95236d5cf0fcdd574c0612cfb0d0f4
Fang Huang <fanghuang1992@gmail.com>
2015-04-10 05:20:36 -0400

    update

--- project/urls.py
+++ project/urls.py
@@ -3,6 +3,6 @@ from project import views
 
 
 urlpatterns = patterns('',
-                       url(r'^create/$', views.ProposalCreateView.as_view(), name='create_proposal'),
-                       url(r'^detail/(?P<pk>[-_\d]+)/$', views.ProjectDetailView.as_view(), name='project_detail'),
-                       )
+	url(r'^create/$', views.ProposalCreateView.as_view(), name='create_proposal'),
+	url(r'^detail/(?P<pk>[-_\d]+)/$', views.ProjectDetailView.as_view(), name='project_detail'),
+)
