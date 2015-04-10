fdd50a056627fd8d5d17d4ada82a0fa9cdd69826
Fang Huang <fanghuang1992@gmail.com>
2015-04-09 22:26:42 -0400

    fix login page ui

--- TJIPMS/urls.py
+++ TJIPMS/urls.py
@@ -3,12 +3,11 @@ from django.contrib import admin
 from TJIPMS import views
 
 urlpatterns = patterns('',
-                       url(r'^$', views.HomeView.as_view(), name='index'),
-                       url(r'^admin/', include(admin.site.urls)),
-                       url(r'^account/', include('account.urls')),
-                       url(r'^tjadmin/', include('tjadmin.urls')),
-                       url(r'^project/', include('project.urls')),
-                       url(r'^announcement/', include('announcement.urls')),
-                       url(r'^i18n', views.language_switch, name="language_switch"),
-
-                       )
+	url(r'^$', views.HomeView.as_view(), name='index'),
+	url(r'^admin/', include(admin.site.urls)),
+	url(r'^account/', include('account.urls')),
+	url(r'^tjadmin/', include('tjadmin.urls')),
+	url(r'^project/', include('project.urls')),
+	url(r'^announcement/', include('announcement.urls')),
+	url(r'^i18n', views.language_switch, name="language_switch"),
+)
